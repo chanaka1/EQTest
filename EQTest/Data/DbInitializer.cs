44c6857b1aabd310078205ee5616525f69c2ab20
Chanaka Jayarathna <chanaka.j@evicio.com>
2018-04-06 13:52:04 +0530

    data added
    Signed-off-by: Chanaka Jayarathna <chanaka.j@evicio.com>

--- EQTest/Data/DbInitializer.cs
+++ EQTest/Data/DbInitializer.cs
@@ -53,8 +53,77 @@ namespace EQTest.Data
             var created = _dbContext.Database.EnsureCreated();
             if (created)
             {
-               // Seed();
+                Seed();
             }
+        }
+
+        private void Seed()
+        {
+            _dbContext.Locations.AddRange(new Models.Location
+            {
+                Name = "Lot1",
+                Latitude = "65.262626",
+                Longitude = "54.654555",
+                LastModifiedBy = 1,
+                LastModifiedDate = DateTime.UtcNow
+            });
+
+            _dbContext.Lots.AddRange(new Models.Lot
+            {
+                Name = "Lot1",
+                Number = "1",
+                City = "Colombo",
+                Color = "Color1",
+                Zip = "11850",
+                StallCount = 25,
+                StallSequence = "2",
+                TimePeriod = 3,
+                IsOverFolowLot = false,
+                Latitude = "65.262626",
+                Longitude = "54.654555",
+                LastModifiedBy = 1,
+                LastModifiedDate = DateTime.UtcNow
+            });
+
+            _dbContext.SaveChanges();
+            
+            _dbContext.LotLocations.AddRange(new Models.LotLocation
+            {
+                LocationId = 1,
+                LotId = 1,
+                LastModifiedBy = 1,
+                LastModifiedDate = DateTime.UtcNow
+            });
+
+            _dbContext.Permits.AddRange(new Models.Permit
+            {
+                Number = "P1555",
+                Name = "Permit 1",
+                Price = 0,
+                GLCode = "GL520",
+                Color = "",
+                Map = "",
+                Description = "",
+                VisibleToPublicFrom = DateTime.UtcNow,
+                VisibleToPublicTo = DateTime.UtcNow.AddYears(2),
+                PermitValidity = Models.PermitValidity.ValidTo,
+                ValidDate1 = DateTime.UtcNow,
+                ValidDate2 = DateTime.UtcNow.AddYears(2),
+                AllocatedStallNos = 20,
+                LastModifiedBy = 1,
+                LastModifiedDate = DateTime.UtcNow
+            });
+
+            _dbContext.SaveChanges();
+
+
+            _dbContext.PermitLots.AddRange(new Models.PermitLot
+            {
+                LotId = 1,
+                PermitId = 1
+            });
+
+            _dbContext.SaveChanges();
         }
     }
 }
